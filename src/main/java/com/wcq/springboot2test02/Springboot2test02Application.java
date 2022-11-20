a7306e85386d2af9dd98250d6b793af9fb7800d0
wucanqin <1611743891@qq.com>
2022-11-20 13:51:09 +0800

    init

---
+++ src/main/java/com/wcq/springboot2test02/Springboot2test02Application.java
@@ -0,0 +1,13 @@
+package com.wcq.springboot2test02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2test02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2test02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/wcq/springboot2test02/Springboot2test02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcq.springboot2test02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2test02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
